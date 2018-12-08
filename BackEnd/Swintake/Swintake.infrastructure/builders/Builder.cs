e48eb0cf7f97b55abcddf61d44447fd8f0dc71e7
Caroline <caroline.callens90@gmail.com>
2018-12-08 12:54:09 +0100

    testing with a new project

--- BackEnd/Swintake/Swintake.infrastructure/builders/Builder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Swintake.infrastructure.builders
-{
-    public abstract class Builder<T>
-    {
-        public abstract T Build();
-        // refactor naar implementatie met return new<T> (this)
-    }
-}
--- BackEnd/Swintake/Swintake/Program.cs
+++ BackEnd/Swintake/Swintake/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Swintake.api
+namespace SwinTake
 {
     public class Program
     {
