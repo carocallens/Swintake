bc2ecbef30606ea6e26f43f8f07723a35f73d435
Caroline <caroline.callens90@gmail.com>
2018-12-08 14:05:20 +0100

    added back end to test if build still works

---
+++ FrontEnd/Swintake/src/app/feature/feature.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoreModule } from '../core/core.module';
+import { RoutingModule } from '../routing/routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoreModule,
+    RoutingModule
+  ]
+})
+export class FeatureModule { }
